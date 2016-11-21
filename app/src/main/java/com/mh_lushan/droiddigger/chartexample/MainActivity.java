c95e9b943f088841d6ad5b7a2203814f2d415bdf
Mihodi Hasan Lushan <mihodihasan@gmail.com>
2016-11-21 22:01:17 +0600

    Lushan

--- app/src/main/java/com/mh_lushan/droiddigger/chartexample/MainActivity.java
+++ app/src/main/java/com/mh_lushan/droiddigger/chartexample/MainActivity.java
@@ -3,16 +3,10 @@ package com.mh_lushan.droiddigger.chartexample;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import java.util.ArrayList;
 import java.util.List;
-
-import lecho.lib.hellocharts.model.Line;
-import lecho.lib.hellocharts.model.LineChartData;
 import lecho.lib.hellocharts.model.PieChartData;
-import lecho.lib.hellocharts.model.PointValue;
 import lecho.lib.hellocharts.model.SliceValue;
-import lecho.lib.hellocharts.view.LineChartView;
 import lecho.lib.hellocharts.view.PieChartView;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,13 +23,7 @@ public class MainActivity extends AppCompatActivity {
         values.add(new SliceValue(20,Color.WHITE,1));
         values.add(new SliceValue(40,Color.GREEN,1));
 
-        //In most cased you can call data model methods in builder-pattern-like manner.
-//        Line line = new Line(values).setColor(Color.BLUE).setCubic(true);
-//        SliceValue lines = new SliceValue();
-//        lines.add(line);
-
         PieChartData data = new PieChartData(values);
-//        data.setValues(data);
 
         PieChartView chart = (PieChartView) findViewById(R.id.chart);
         chart.setPieChartData(data);
